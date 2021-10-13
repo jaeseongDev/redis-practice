76c53859461fa292a24621dfa4576982f4ee374f
jaeseongDev <qkrwotjd1445@naver.com>
2021-10-13 22:29:35 +0900

    feat: Redis 코드 추가

--- src/main/java/com/example/loadtest/controller/TestController.java
+++ src/main/java/com/example/loadtest/controller/TestController.java
@@ -4,6 +4,8 @@ import com.example.loadtest.domain.Comment;
 import com.example.loadtest.domain.CommentRepository;
 import com.example.loadtest.domain.Post;
 import com.example.loadtest.domain.PostRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -17,6 +19,7 @@ import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,5 +48,42 @@ public class TestController {
         ValueOperations<String, String> stringValueOperations = stringRedisTemplate.opsForValue();
         String result = stringValueOperations.get(key);
         return result;
+    }
+
+    @GetMapping("/api/comment/save")
+    public String save() throws JsonProcessingException {
+        // 1. DB에 저장 로직
+        Comment temp = new Comment("댓글 내용", "댓글쓴이");
+        Comment comment = commentRepository.save(new Comment("댓글 내용", "댓글쓴이"));
+
+        // 2. Redis에 데이터 저장 or 갱신 (key, value)
+        ValueOperations<String, String> stringValueOperations = stringRedisTemplate.opsForValue();
+        String key = "comment" + comment.getId(); // key : 해당 값을 알아볼 수 있을만한 고유값
+        ObjectMapper mapper = new ObjectMapper();
+        String value = mapper.writeValueAsString(comment);
+        stringValueOperations.set(key, value); // value : 데이터를 조회하고 싶은 형태
+        return value;
+    }
+
+    @GetMapping("/api/comment")
+    public String find(@RequestParam("id") Long commentId) throws JsonProcessingException {
+        // 1. Redis에 해당 key값으로 찾았을 때, 데이터가 존재하는 지 확인
+        ValueOperations<String, String> stringValueOperations = stringRedisTemplate.opsForValue();
+        String key = "comment" + commentId;
+        String value = stringValueOperations.get(key);
+        Comment comment = null;
+        // 2. 존재한다면 Redis로부터 값을 조회해와서 사용자한테 바로 return하고 끝!
+        if (value != null) {
+            ObjectMapper mapper = new ObjectMapper();
+            comment = mapper.readValue(value, Comment.class);
+        }
+
+        // if) 존재하지 않는다면, DB로부터 조회해와서 Redis에 값을 저장해놓고, 사용자한테 return하고 끝!
+        if (value == null) {
+            Optional<Comment> possibleComment = commentRepository.findById(commentId);
+            comment = possibleComment.get();
+        }
+        System.out.println(comment);
+        return "ok";
     }
 }
